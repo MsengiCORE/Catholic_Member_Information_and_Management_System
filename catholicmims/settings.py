af3d29f6e88735e6b20df321f21ff110f4f428c6
MsengiCORE <MsengiN17@outlook.com>
2026-09-02 15:49:00 +0300

    documentations

--- catholicmims/settings.py
+++ catholicmims/settings.py
@@ -119,7 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Email
@@ -130,3 +134,7 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+# Media files (user-uploaded files)
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
