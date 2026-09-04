31eb16693e4ff9a38721e408f672f3e5e800a866
MsengiCORE <MsengiN17@outlook.com>
2026-09-04 11:29:21 +0300

    base app and djangi + tailwind configurations

--- catholicmims/settings.py
+++ catholicmims/settings.py
@@ -38,7 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-Party apps
+    'tailwind',
+    'theme',
 ]
+
+TAILWIND_APP_NAME = "theme"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
